a0d5ed54921d30b41a0f0ee26a38b7bf6b104daa
mike dupont <mike.dupont@introspector.local>
2024-12-27 22:56:52 -0500

    trying to fix the permissions of ecr

---
+++ transfer.sh
@@ -0,0 +1,4 @@
+docker pull h4ckermike/swarms-mcs:experimental
+docker tag h4ckermike/swarms-mcs:experimental 916723593639.dkr.ecr.us-east-2.amazonaws.com/swarms/mcs:latest
+docker push 916723593639.dkr.ecr.us-east-2.amazonaws.com/swarms/mcs:latest
+
